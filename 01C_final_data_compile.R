e3df8bbe28a25c4cb123c280de06dc48a55c9b18
Kate Miller <kathryn_miller@nps.gov>
2024-03-13 16:33:26 -0400

    fixed lags and rolls

--- 01C_final_data_compile.R
+++ 01C_final_data_compile.R
@@ -7,10 +7,9 @@ library(zoo) # for rollmean
 
 # Simplifying dataset to only include variables of interest
 # -- Choosing DayMet-only derived climate data
-fulld <- read.csv("./data/ACAD_plot_core_climate_full_data.csv") |> filter(Year >= 1980)
+fulld <- read.csv("./data/ACAD_plot_core_climate_full_data.csv") |> filter(Year >= 1980) |> 
+  filter(!is.na(coreID))
 
-head(fulld)
-table(fulld$Group_1)
 fulld <- fulld |> mutate(forest_type = 
                            case_when(Group_1 == "Laurentian - Acadian Acidic Swamp" ~ "FSWP",
                                      Group_1 == "Northern Hardwood - Hemlock Hardwood Forest" ~ "NHWD",
@@ -57,20 +56,27 @@ simpd2 <- simpd |>
   ungroup() |> 
   select(Plot_Name:pH, ppt_4:ppt_9, ppt_gs, tmax_gs, tmin_wint, 
          SPEI01_1:SPEI03_12)
-
-#dat$SPEI03_8_roll5 <- rollmean(stats::lag(dat$SPEI03_8, k = 5), k = 5, fill = NA)
+head(simpd2)
+names(simpd2)
+?pmin
 
 # Calculate a rolling average to capture cumulative effects of previous dry or wet years
-roll_fun <- function(dat, x, roll){
-  dat1 <- dat |> select(Plot_Name, coreID, Year, all_of(x)) |> 
+var = "SPEI01_3"
+roll = 5
+
+roll_fun <- function(dat = simpd2, var, roll){
+  #xcol <- paste0(var, "_roll", roll)
+  dat1 <- dat |> select(Plot_Name, coreID, Year, all_of(var)) |> 
     arrange(Plot_Name, coreID, Year) |> 
     group_by(Plot_Name, coreID) |> 
-    mutate("{x}_roll{roll}" := 
-             zoo::rollmean(!!sym(x), k = roll, 
+    mutate(lag_x = dplyr::lag(!!sym(var), n = 1)) |> 
+    mutate("{var}_roll{roll}" := 
+             zoo::rollmean(lag_x, k = roll, 
                       align = 'right', # calcs previous window 
                       fill = NA)) |> 
-    ungroup() |> data.frame()
-  col = c(paste0(x, "_roll", roll))
+    ungroup() |> select(-lag_x) |> 
+    data.frame()
+  col = c(paste0(var, "_roll", roll))
   dat2 <- data.frame(dat1[,ncol(dat1)])
   names(dat2) <- col
   return(dat2)
@@ -88,25 +94,38 @@ lag_fun <- function(dat, x, lag){
   return(dat2)
 }
 
-vars <- c("SPEI01_4", "SPEI01_5", "SPEI01_6", "SPEI01_7", "SPEI01_8", "SPEI01_9",
-          "SPEI03_4", "SPEI03_5", "SPEI03_6", "SPEI03_7", "SPEI03_8", "SPEI03_9",
-          "NO3", "SO4", "pH", "tmin_wint", "tmax_gs", "ppt_gs", "ppt_8")
+# head(roll_fun(simpd2, "SPEI03_9", 2))
+# head(lag_fun(simpd2, "SPEI03_9", 1))
+# head(simpd2$SPEI03_9)
 
+vars <- c("SPEI01_4", "SPEI01_5", "SPEI01_6", "SPEI01_7", 
+          "SPEI01_8", "SPEI01_9", "SPEI01_10", 
+          "SPEI03_4", "SPEI03_5", "SPEI03_6", "SPEI03_7", 
+          "SPEI03_8", "SPEI03_9", "SPEI03_10",
+          "NO3", "SO4", "pH", "tmin_wint", "tmax_gs", "ppt_gs", 
+          "ppt_8", "ppt_4")
 rolls <- c(2:5)
+
 roll_df <- data.frame(vars = rep(vars, each = length(rolls)),
                       rolls = rep(rolls, length(vars)))
-names(simpd2)
-core_rolls <- cbind(simpd2,
-                    purrr::map2_dfc(roll_df$vars, roll_df$rolls, 
-                                    ~roll_fun(dat = simpd2, x = .x, roll = .y))
-)
+
+
+core_rolls <- cbind(simpd2, ##|> select(Plot_Name, coreID, Year),
+                    map2(roll_df$vars, roll_df$rolls,
+                       ~roll_fun(simpd2, var = .x, roll = .y)) |> list_cbind())
 
 core_lagrolls <- cbind(core_rolls, 
-                       purrr::map_dfc(vars, ~lag_fun(dat = core_rolls, ., 1)))
+                       map(vars, 
+                           ~lag_fun(core_rolls, var = ., lag = 1)) |> 
+                         list_cbind())
 
-names(core_lagrolls)
+ggplot(core_lagrolls |> filter(Plot_Name == "ACAD-101"),
+       aes(x = Year, y = SPEI03_8_roll5)) + geom_point() + geom_smooth()
+
+
 table(core_lagrolls$Crown_Class)
 table(core_lagrolls$Year) #1980 to 2022
+hist(core_lagrolls$Year)
 write.csv(core_lagrolls, "./data/ACAD_final_core_data_20240311.csv", row.names = F)
 
 
